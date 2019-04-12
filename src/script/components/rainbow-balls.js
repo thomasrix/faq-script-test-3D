ae15ea58293bede76861d974d8072d7337565d86
Thomas Rix <trix@dr.dk>
2019-04-12 11:12:49 +0200

    Fixing dem rainbow balls

--- src/script/components/rainbow-balls.js
+++ src/script/components/rainbow-balls.js
@@ -8,14 +8,14 @@ require('three/examples/js/controls/OrbitControls');
 export default class RainbowBalls{
     constructor(){
         this.props = {
-            width:1920,
-            height:1080
+            width:800,
+            height:450
         }
         this.init(this.props);
     }
     init(props){
         let {width, height} = props;
-        let container = select('[canvas-tester-entry]');
+        let container = select('[canvas-tester-entry-3d]');
         let canvas = create('canvas', container, 'faq-ct-canvas');
         canvas.width = width;
         canvas.height = height;
@@ -35,7 +35,8 @@ export default class RainbowBalls{
                         u, v
                     ],
                     radius:40,
-                    color:`hsl(${lerp(0, 360, u)}, ${lerp(10, 100, (v+u)/2)}%, ${lerp(20, 95, (v+u)/2)}%)`
+                    color:`hsl(${lerp(0, 360, u)}, 50%, 50%)`
+                    // color:`hsl(${lerp(0, 360, u)}, ${lerp(10, 100, (v+u)/2)}%, ${lerp(20, 95, (v+u)/2)}%)`
                 })
             }
         }
@@ -44,17 +45,87 @@ export default class RainbowBalls{
     build(props){
         let {width, height, context} = props;
         let margin = width * 0.1;
-        let c = context;
-
-/*         let points = this.createGrid();
-        points.forEach(({position, radius, color}) =>{
+        
+        const renderer = new THREE.WebGLRenderer({
+            context
+        });
+        // WebGL background color
+        renderer.setClearColor('hsl(0, 0%, 60%)', 0);
+        renderer.setSize(width, height);
+        
+        // Setup a camera
+        // const camera = new THREE.OrthographicCamera();
+        const camera = new THREE.PerspectiveCamera(40, width / height, 0.1, 100);
+        camera.position.z = 12;
+        // camera.position.y = 0;
+        // Setup camera controller
+        const controls = new THREE.OrbitControls(camera);
+        
+        // Setup your scene
+        const scene = new THREE.Scene();
+        
+        const box = new THREE.BoxGeometry(1, 1, 1);
+        const sphere = new THREE.SphereGeometry( 0.5, 32, 32 );
+        
+        const group = new THREE.Group();
+        
+        const points = this.createGrid();
+        points.forEach(({position, color}) => {
+            const material = new THREE.MeshStandardMaterial({
+                color: color,
+                metalness:0.1,
+                roughness:0.4,
+                wireframe:false
+            });
             const [u, v] = position;
-            const x = lerp(margin, width - margin, u);
-            const y = lerp(margin, height-margin, v);
-            context.fillStyle = color;
-            context.beginPath();
-            context.arc(x, y, radius, 0, Math.PI * 2);
-            context.fill();
+            const x = lerp(-6, 6, u);
+            const y = lerp(-3, 3, v);
+            const mesh = new THREE.Mesh(
+                sphere,
+                material
+                );
+                mesh.position.set(
+                    x,
+                    y,
+                    0
+                    );
+                    group.add(mesh);
         })
- */    }
-}
+                
+                scene.add(group);
+                // camera.lookAt(new THREE.Vector3(0, 0, 20));
+                
+                // Update the camera
+                // camera.updateProjectionMatrix();
+                
+                // Specify an ambient/unlit colour
+                scene.add(new THREE.AmbientLight('hsl(0, 0%, 70%)'));
+                let d_light = new THREE.PointLight( 0xffffff, 0.5, 20, 20);
+                
+                d_light.castShadow = true;
+                d_light.position.set( 1, 5, 0 );
+                
+                scene.add(d_light);
+                
+                
+                /*         
+                let points = this.createGrid();
+                points.forEach(({position, radius, color}) =>{
+                    const [u, v] = position;
+                    const x = lerp(margin, width - margin, u);
+                    const y = lerp(margin, height-margin, v);
+                    context.fillStyle = color;
+                    context.beginPath();
+                    context.arc(x, y, radius, 0, Math.PI * 2);
+                    context.fill();
+                })
+                */    
+                const render = ()=>{
+                    //    console.log('render')
+                    controls.update();
+                    renderer.render(scene, camera);
+                    requestAnimationFrame(render);
+                }
+                render();
+            }
+        }
